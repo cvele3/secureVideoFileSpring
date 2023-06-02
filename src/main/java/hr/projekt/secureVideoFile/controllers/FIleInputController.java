307102c1dfa8d1f588b27b12d0f5423e5cf2b4dd
cvele3 <cvetkojakov@gmail.com>
2023-06-02 16:50:29 +0200

    react and file streaming adaptations

--- src/main/java/hr/projekt/secureVideoFile/controllers/FIleInputController.java
+++ src/main/java/hr/projekt/secureVideoFile/controllers/FIleInputController.java
@@ -8,10 +8,12 @@ import jakarta.validation.Valid;
 import javafx.util.Pair;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import java.io.File;
 import java.io.IOException;
@@ -64,7 +66,7 @@ public class FIleInputController {
 
     @Operation(summary = "Convert input file", description = "Convert input file to video using password based encryption")
     @PostMapping(path = PathParamConstants.UPLOAD_FILE_AND_GET_URL)
-    public ResponseEntity<String> uploadFileAndGetURL(@RequestParam("file") MultipartFile file, @RequestParam("password") String password, @RequestParam("name") String name) throws IOException {
+    public ResponseEntity<String> uploadFileAndGetURL(@RequestParam("file") MultipartFile file, @RequestParam("password") String password, @RequestParam("name") String name, @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader) throws IOException {
         log.info("uploadFileAndGetURL endpoint entered");
 
         String URL = fileConversionManager.convertFileToSignedVideoAndGetURL(file, password, name);
@@ -98,14 +100,21 @@ public class FIleInputController {
 
     @Operation(summary = "Convert video to file", description = "Convert video from drive to original file")
     @PostMapping(path = PathParamConstants.RETRIEVE_FILE_FROM_URL)
-    public ResponseEntity<String> retrieveFileFromURL(@RequestParam("password") String password, @RequestParam("URL") String URL) throws IOException {
+    public ResponseEntity<StreamingResponseBody> retrieveFileFromURL(@RequestParam("password") String password, @RequestParam("URL") String URL) throws IOException {
         log.info("retrieveFile endpoint entered");
 
-        byte[] fileContent= fileConversionManager.convertSignedVideoToFileUsingURL(password, URL);
+        byte[] fileContent = fileConversionManager.convertSignedVideoToFileUsingURL(password, URL);
 
+        StreamingResponseBody responseBody = outputStream -> {
+            outputStream.write(fileContent);
+            outputStream.flush();
+        };
 
-        return ResponseEntity.ok(fileContent.toString());
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"file.bin\"")
+                .body(responseBody);
     }
+
 
     @Operation(summary = "Delete user videos", description = "Delete user videos using user info")
     @DeleteMapping(path = PathParamConstants.DELETE_USER_VIDEOS)
